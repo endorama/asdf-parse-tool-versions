266a3be78815faeb453014c13e4fcf5f2a622439
Edoardo Tenani <edoardo.tenani@pm.me>
2025-10-27 23:23:47 +0100

    add test

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -8,13 +8,15 @@ test('parse a file', async () => {
   const tools = await parseToolVersions(path.join(__dirname, 'tool-versions'))
 
   // console.log(tools)
-  expect(tools.size).toBe(3)
+  expect(tools.size).toBe(4)
   expect(tools.has('golang')).toBeTruthy()
   expect(tools.get('golang')).toBe('1.12.5')
   expect(tools.has('ruby')).toBeTruthy()
   expect(tools.get('ruby')).toBe('2.7.0')
   expect(tools.has('golangci_lint')).toBeTruthy()
   expect(tools.get('golangci_lint')).toBe('1.64.8')
+  expect(tools.has('terraform')).toBeTruthy()
+  expect(tools.get('terraform')).toBe('1.13.4')
 })
 
 test('toEnvVarName converts tool names to environment variable names', () => {
