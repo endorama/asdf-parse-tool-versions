b0f862689de9d62dd5c5fab5a78f2d8bc24fe3e9
Edoardo Tenani <edoardo.tenani@pm.me>
2025-10-27 22:30:20 +0100

    enhance tests
    Test for env and JSON values, instead of just printing them

---
+++ .github/workflows/scripts/test/test-envs.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+set -eou pipefail
+
+checkenv() {
+  local envname
+  envname="$1"
+  local expected
+  expected="$2"
+
+  got=$(printenv "$envname")
+  if [[ "${got:-}" != "$expected" ]]; then
+    echo "$envname differs from expectations: got: '${got:-missing}' expected: '$expected'"
+    return 2
+  fi
+  return 0
+}
+
+errors=0
+if ! checkenv "GOLANG_VERSION" "1.12.5"; then
+  errors=$((errors + 1))
+fi
+if ! checkenv "RUBY_VERSION" "2.7.0"; then
+  errors=$((errors + 1))
+fi
+if ! checkenv "GOLANGCI_LINT_VERSION" "1.64.8"; then
+  errors=$((errors + 1))
+fi
+
+if [[ ! $errors -eq 0 ]]; then
+  echo "Some error occurred, test failed"
+  exit 1
+fi
